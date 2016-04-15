72833c98b98fc2e8e89ffdfe3b82c90e4eff21ec
Daniel Kirov <kirov.daniel@gmail.com>
2016-04-15 23:03:43 +0100

    Add basic mavin configuration and main class.

---
+++ catan/src/main/java/com/boardgames/catan/App.java
@@ -0,0 +1,25 @@
+package com.boardgames.catan;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+
+public class App extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> fut) {
+        vertx
+            .createHttpServer()
+            .requestHandler(r -> {
+                r.response().end("<h1>Zdraveite gei4enca</h1>");
+            })
+            .listen(8080, result -> {
+                if (result.succeeded()) {
+                    fut.complete();
+                } else {
+                    fut.fail(result.cause());
+                }
+            });
+    }
+
+}
