41a8b943e5f25be84e890c1f2696d39a9c76715b
Daniel Kirov <kirov.daniel@gmail.com>
2016-04-16 13:07:12 +0100

    Add basic websocket server.

--- catan/src/main/java/com/boardgames/catan/App.java
+++ catan/src/main/java/com/boardgames/catan/App.java
@@ -2,6 +2,7 @@ package com.boardgames.catan;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.net.NetServer;
 
 
 public class App extends AbstractVerticle {
@@ -20,6 +21,18 @@ public class App extends AbstractVerticle {
                     fut.fail(result.cause());
                 }
             });
+        
+        NetServer server = vertx.createNetServer();
+        server.connectHandler(socket -> {
+          socket.handler(buffer -> {
+        	System.out.println(buffer);
+        	String host = String.format("IP-to ti bi trqbvalo da e %s", socket.remoteAddress().host());
+            socket.write(host).close();
+          });
+        });
+        server.listen(8081);
+        
+        
     }
 
 }
