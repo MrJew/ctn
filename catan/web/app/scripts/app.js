9898221d042376ac745fa159bca8590412428b27
Atanas Bozhkov <bozhkov.atanas@gmail.com>
2016-04-16 14:30:15 +0100

    Added yo scaffolding

---
+++ catan/web/app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ctnApp
+ * @description
+ * # ctnApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ctnApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
