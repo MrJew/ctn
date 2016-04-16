053824e5f953d79496769813887fc56a449d23e3
MrJew <kouzmov@gmail.com>
2016-04-16 12:53:16 +0100

    revert to old version of app

--- catan/src/main/java/com/boardgames/catan/App.java
+++ catan/src/main/java/com/boardgames/catan/App.java
@@ -1,24 +1,25 @@
-import org.vertx.java.core.Handler;
-import org.vertx.java.core.http.HttpServerRequest;
-import org.vertx.java.platform.Verticle;
- 
-import java.util.Map;
- 
-public class App extends Verticle {
- 
-  public void start() {
-    int port = Integer.parseInt(System.getenv("OPENSHIFT_VERTX_PORT"));
-    String ip = System.getenv("OPENSHIFT_VERTX_IP");
-    vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest req) {
-        System.out.println("Got request: " + req.uri());
-        System.out.println("Headers are: ");
-        for (Map.Entry<String, String> entry : req.headers()) {
-          System.out.println(entry.getKey() + ":" + entry.getValue());
-        }
-        req.response().headers().set("Content-Type", "text/html; charset=UTF-8");
-        req.response().end("<html><body><h1>Hello from Java on vert.x!</h1></body></html>");
-      }
-    }).listen(port, ip);
-  }
+package com.boardgames.catan;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+
+public class App extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> fut) {
+        vertx
+            .createHttpServer()
+            .requestHandler(r -> {
+                r.response().end("<h1>Zdraveite gei4enca</h1>");
+            })
+            .listen(8080, result -> {
+                if (result.succeeded()) {
+                    fut.complete();
+                } else {
+                    fut.fail(result.cause());
+                }
+            });
+    }
+
 }
