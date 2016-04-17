bab87c8c67e62a7d5e7a2c52a8ca67aa9f3b5ee4
Atanas Bozhkov <bozhkov.atanas@gmail.com>
2016-04-17 18:07:50 +0100

    select kind of works

--- catan/web/app/scripts/controllers/main.js
+++ catan/web/app/scripts/controllers/main.js
@@ -9,13 +9,13 @@
  */
 angular.module('webApp')
   	.controller('MainCtrl', function ($scope, $mdSidenav, $http, $websocket, $mdDialog) {
-  		
+
   		$scope.toggleSidenav = function(menuId) {
   			$mdSidenav(menuId).toggle().then(function(){
   				console.log('clicked:'+menuId);
   			});
   		};
-  		
+
   		$scope.showAddDialog = function(modalId){
   			$mdDialog.show({
   				scope: $scope,
@@ -24,53 +24,49 @@ angular.module('webApp')
   			});
   		};
   		/*$scope.addSession = function(){
-  			
+
   			var data = $.param({
   	                game: "Settlers of Cattan",
   	                players: 4,
   	        });
-  			
+
   			$http.post("http://localhost:8080/createSession", data).success(function(data, status){
   				console.log("New session created:\n" + data)
   			});
   		};*/
-  		
+
   		var ws = $websocket('ws://localhost:8081');
   		ws.binaryType = "arraybuffer";
   		ws.onMessage(function(message) {
   	        console.log("DATA" + message.data);
   	     });
-  		
+
   		ws.onError(function(message){
   			console.log("Error:" + message);
   		});
-  		
+
   		ws.onClose(function(message){
   			console.log("Close:" + message);
   		});
-  		
+
   		ws.onOpen(function(message){
   			console.log("Open:" + message);
   		});
-  		
+
   		$scope.rollDice = function(){
   			ws.send(JSON.stringify({ action: 'roll_dice' }));
   		};
-  		
+
   		this.awesomeThings = [
 	      'HTML5 Boilerplate',
 	      'AngularJS',
 	      'Karma'
 	    ];
   })
-  .controller('AddModalCtrl', function($scope,$mdDialog){
-	  
+  .controller('AddModalCtrl', function($scope, $mdDialog){
 	  $scope.gameTypes = [
 	            			{'name':"Sttlrs Ctn"},
 	            			{'name':"Cities and Knights"},
 	            			{'name':"Seafares"}
 	  ];
-	  
-	  $scope.dialog = {};
-	  $scope.dialog.type = "Sttlrs Ctn";
   });
