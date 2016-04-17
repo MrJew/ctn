1b52f11af2050bbaa60ff6e9766207bd0a8a8ae7
MrJew <kouzmov@gmail.com>
2016-04-17 13:19:36 +0100

    websockets prototype done

--- catan/src/main/java/com/boardgames/catan/controller/CatanServer.java
+++ catan/src/main/java/com/boardgames/catan/controller/CatanServer.java
@@ -2,8 +2,11 @@ package com.boardgames.catan.controller;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.buffer.impl.BufferFactoryImpl;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.ServerWebSocket;
 import io.vertx.ext.web.Router;
@@ -20,9 +23,35 @@ public class CatanServer extends AbstractVerticle {
 		
 		Router router = catanServerController.getRouter();
 		
-		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-		vertx.createHttpServer().websocketHandler(this::test).listen(8081);
+		final EventBus eventBus = vertx.eventBus();
 		
+		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+		vertx.createHttpServer().websocketHandler(new Handler<ServerWebSocket>(){
+			
+			
+			@Override
+			public void handle(final ServerWebSocket ws) {
+				
+				String id = ws.textHandlerID();
+				System.out.println("Registering new ws connection for id: "+id);
+				
+				ws.closeHandler(new Handler<Void>(){
+					@Override
+					public void handle(Void event) {
+						System.out.println("Closing hanlder");
+					}
+				});
+				
+				ws.handler(new Handler<Buffer>(){
+					@Override
+					public void handle(Buffer event) {
+						System.out.println(event.toString());
+						eventBus.send(id, "Dice roll 6");
+					}
+				});
+			}
+		
+		}).listen(8081);
 	}
 	
 	private void test(ServerWebSocket request){
